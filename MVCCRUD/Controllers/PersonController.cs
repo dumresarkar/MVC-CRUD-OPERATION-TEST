ebb6464d5aac75abf896c1f7c2f3a8fc151e1d56
Dumre Sabin <70896989+dumresarkar@users.noreply.github.com>
2022-06-26 10:50:03 +0545

    MVCCRUD
    CRUD operation in MVC is the basic operations, where CRUD denotes create, read, update, and delete. But before understanding the basic operations of MVC, first, learn about MVC. MVC is the Model View Controller.

---
+++ MVCCRUD/Controllers/PersonController.cs
@@ -0,0 +1,73 @@
+﻿using MVCCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCRUD.Controllers
+{
+    
+    public class PersonController : Controller
+    {
+        Database001Entities Contextmodel = new Database001Entities();
+        public ActionResult Register()
+        {
+            return View("Register");
+        }
+        public ActionResult AddPerson()
+        {
+            PersonModel model = new PersonModel();
+            return View("AddPerson",model);   
+        }
+        
+        [HttpPost]
+        public ActionResult AddPerson(PersonModel model)
+        {
+           
+            Contextmodel.People.Add(new Person() { PersonName = model.PersonName, PersonAddress = model.PersonAddress });
+            Contextmodel.SaveChanges();
+            return View("AddPerson", model);
+        }
+        public  ActionResult DisplayPersons()
+        {
+            
+            var PersonRecords = Contextmodel.People.ToList();
+            return View("DisplayPersons", PersonRecords);
+        }
+        public ActionResult EditPerson(int personid)
+        {
+            var personRec = (from item in Contextmodel.People
+                             where item.PersonId == personid
+                             select item).First();
+            return View("EditPerson",personRec);
+        }
+        [HttpPost]
+        public ActionResult EditPerson(Person model)
+        {
+            var personRec = (from item in Contextmodel.People
+                             where item.PersonId == model.PersonId
+                             select item).First();
+
+            personRec.PersonName = model.PersonName;
+            personRec.PersonAddress = model.PersonAddress;
+
+            Contextmodel.SaveChanges();
+
+            var PersonRecords = Contextmodel.People.ToList();
+            return View("DisplayPersons", PersonRecords);
+        }
+
+        public ActionResult DeletePerson(int personId)
+        {
+
+            var personRec = (from item in Contextmodel.People
+                             where item.PersonId == personId
+                             select item).First();
+            Contextmodel.People.Remove(personRec);
+            Contextmodel.SaveChanges();
+            var PersonRecords = Contextmodel.People.ToList();
+            return View("DisplayPersons", PersonRecords);
+        }
+    }
+}
---
+++ MVCCRUD/Models/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCCRUD.Models
+{
+    public class PersonModel
+    {
+        public string PersonName { get; set; }
+        public string PersonAddress { get; set; }
+    }
+}
